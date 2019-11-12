b98c795880156d6ba233f53b96c2954d3da47d5b
farzaneh <farzaneh@binary.com>
2019-11-12 15:29:21 +0000

    create traits class

--- lib/WebService/Async/Segment/Base/Traits.pm
+++ lib/WebService/Async/Segment/Base/Traits.pm
@@ -1,10 +1,23 @@
-package WebService::Async::Segment::Traits;
+package WebService::Async::Segment::Base::Traits;
 
 use strict;
 use warnings;
 
 ## VERSION
 
+=head1 NAME
 
+WebService::Async::Segment::Base::Traits - represents data for Traits fields
+
+=head1 DESCRIPTION
+
+This is generated based on the documentation in L<>
+
+=cut
+
+for qw(address age avatar birthday company created_at description email first_name gender id last_name name phone title username website){
+    has $_ => { is => 'ro' }
+}
 
 1;
+
