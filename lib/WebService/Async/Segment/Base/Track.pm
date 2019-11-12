27dc308436a1cb5a8e79380c17f2143da99127d7
farzaneh <farzaneh@binary.com>
2019-11-12 15:20:02 +0000

    create track class

--- lib/WebService/Async/Segment/Base/Track.pm
+++ lib/WebService/Async/Segment/Base/Track.pm
@@ -1,9 +1,27 @@
-package WebService::Async::Segment::Track;
+package WebService::Async::Segment::Base::Track;
 
 use strict;
 use warnings;
 
 ## VERSION
 
+use Moo;
+
+## VERSION
+
+=head1 NAME
+
+WebService::Async::Segment::Base::Track - represents data for Track command
+
+=head1 DESCRIPTION
+
+This is generated based on the documentation in L<>
+
+=cut
+
+for qw(event properties){
+    has $_ => { is => 'ro' }
+}
 
 1;
+
