bd07d8069399b76e8df22defa00774792ad58064
farzaneh <farzaneh@binary.com>
2019-11-13 10:23:47 +0000

    add Identify representation class

--- lib/WebService/Async/Segment/Identify.pm
+++ lib/WebService/Async/Segment/Identify.pm
@@ -3,7 +3,27 @@ package WebService::Async::Segment::Identify;
 use strict;
 use warnings;
 
-## VERSION
+use parent qw(WebService::Async::Segment::Base::Identify);
+
+# VERSION
+
+=head1 NAME
+
+WebService::Async::Segment::Identify - represents data for Segment
+
+=head1 DESCRIPTION
+
+=cut
 
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+binary.com C<< BINARY@cpan.org >>
+
+=head1 LICENSE
+
+Copyright binary.com 2019. Licensed under the same terms as Perl itself.
