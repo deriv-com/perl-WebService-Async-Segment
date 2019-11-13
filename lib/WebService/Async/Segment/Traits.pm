8064a42b31a3e3cd71616b9dcc2530597563feee
farzaneh <farzaneh@binary.com>
2019-11-13 10:31:05 +0000

    add Identify representation class, track traits context

--- lib/WebService/Async/Segment/Traits.pm
+++ lib/WebService/Async/Segment/Traits.pm
@@ -3,8 +3,27 @@ package WebService::Async::Segment::Traits;
 use strict;
 use warnings;
 
-## VERSION
+use parent qw(WebService::Async::Segment::Base::Traits);
 
+# VERSION
+
+=head1 NAME
+
+WebService::Async::Segment::Traits - represents data for Segment
+
+=head1 DESCRIPTION
+
+=cut
 
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+binary.com C<< BINARY@cpan.org >>
+
+=head1 LICENSE
+
+Copyright binary.com 2019. Licensed under the same terms as Perl itself.
