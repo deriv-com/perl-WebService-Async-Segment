56f5e49dbd9bc753d5856c00c283ead559cda29b
Mat Sepahi <matt-fs@protonmail.com>
2019-11-13 10:00:14 +0000

    track class finished

--- lib/WebService/Async/Segment/Base/Track.pm
+++ lib/WebService/Async/Segment/Base/Track.pm
@@ -5,23 +5,51 @@ use warnings;
 
 ## VERSION
 
-use Moo;
-
-## VERSION
-
 =head1 NAME
 
 WebService::Async::Segment::Base::Track - represents data for Track command
 
 =head1 DESCRIPTION
 
-This is generated based on the documentation in L<>
+This is generated based on the documentation in L<https://segment.com/docs/spec/track/>
 
 =cut
 
-for qw(event properties){
-    has $_ => { is => 'ro' }
+sub new {
+    my ($class, %args) = @_;
+    Scalar::Util::weaken($args{segment}) if $args{segment};
+    bless \%args, $class;
 }
+
+=head1 METHODS
+
+=head2 event
+
+Boolean	Whether a user is active
+This is usually used to flag an .identify() call to just update the traits but not “last seen.”
+
+=cut
+
+sub event : method { shift->{ active } }
+
+
+=head2 properties
+
+Boolean	Whether a user is active
+This is usually used to flag an .identify() call to just update the traits but not “last seen.”
+
+=cut
+
+sub properties : method { shift->{ properties } }
 
 1;
 
+__END__
+
+=head1 AUTHOR
+
+binary.com C<< BINARY@cpan.org >>
+
+=head1 LICENSE
+
+Copyright binary.com 2019. Licensed under the same terms as Perl itself.
