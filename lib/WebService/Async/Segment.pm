8fa075feb12f333f9733688bda41d1220ab36648
mat-fs <53427463+mat-fs@users.noreply.github.com>
2019-11-28 10:09:24 +0330

    Update lib/WebService/Async/Segment.pm
    Co-Authored-By: Tom Molesworth <tom@binary.com>

--- lib/WebService/Async/Segment.pm
+++ lib/WebService/Async/Segment.pm
@@ -164,7 +164,7 @@ It returns a L<Future> object that should be taken care of by the caller.
 sub method_call {
     my ($self, $method, %args) = @_;
 
-    $args{sent_at} = delete($args{sent_at}) // Time::Moment->now_utc->to_string();
+    $args{sent_at} ||= Time::Moment->now_utc->to_string();
     $args{context}->{library}->{name}    = ref $self;
     $args{context}->{library}->{version} = $VERSION;
 
